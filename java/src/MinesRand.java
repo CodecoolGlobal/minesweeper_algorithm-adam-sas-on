42b0d793e2a9093212b4f15e69f00bb078062471
Adam Sz <adamsz303@gmail.com>
2020-01-22 14:01:54 +0100

    Add MinesRand class for random requirements

---
+++ java/src/MinesRand.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class MinesRand {
+	private static Random rand;
+
+	MinesRand(){
+		rand = new Random();
+	}
+
+	public int[] choices(int[] list, final int size){
+
+	}
+	public int[] sample(int[] list, final int size){
+
+	}
+}
